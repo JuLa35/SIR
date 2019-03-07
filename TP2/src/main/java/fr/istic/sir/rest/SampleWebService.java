6b7840b7862856622c50818cb2af4e9f8e410b24
JuLa35 <43664937+JuLa35@users.noreply.github.com>
2019-03-07 16:50:17 +0100

    Update SampleWebService.java

--- TP2/src/main/java/fr/istic/sir/rest/SampleWebService.java
+++ TP2/src/main/java/fr/istic/sir/rest/SampleWebService.java
@@ -1,12 +1,16 @@
 package fr.istic.sir.rest;
 
+import java.util.List;
+
 import javax.servlet.annotation.WebServlet;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+import tddmt.*;
+
+@Path("/home")
 public class SampleWebService {
 	
     @GET
@@ -14,5 +18,17 @@ public class SampleWebService {
     public String sayHello() {
         return "Hello, how are you?";
     }
+    
+    @GET
+	@Path("/sondage")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Home getHome() {
+		Home h = new Home();
+		h.setTitle("Tro Dtruk Dan Ma TÃªt");
+		List<Sondage> ls = null;
+		// lr = toute les reunions de la bdd
+		h.setSondages(ls);
+		return h;
+	}
     
 }
